54a8cc8376f7e84042e2f4bb062fee57b0f49d8d
clean-command-1851 <ed7901979t@gmail.com>
2019-12-03 14:17:19 +0000

    completing scraper files

--- lib/piano_music_schools/scraper.rb
+++ lib/piano_music_schools/scraper.rb
@@ -1,24 +1,23 @@
 class PianoMusicSchools::Scraper
+   
   
   def self.get_piano_sites        
-# composers = []                    
+                   
     base_url = 'http://gnoted.com/9-websites-to-play-piano-online-for-free/'
-    #main_url = "#{base_url}/jobs?category=programming&type=&region=newyork" 
-    #@data = self.data_scraper(base_url)  original
     Nokogiri::HTML.parse(open('http://gnoted.com/9-websites-to-play-piano-online-for-free/')).title.strip   
     @data = self.data_scraper(base_url) #new
     @list_piano_site_description=@data.css("h3") 
-    #**********************************************
-    #@list_piano_paragraph_description=@data.css('p[style=text-align left;]')
-    #**********************************************
+    
     
     @list_piano_site_description.each do |rep|
       name=rep.css('a').text.strip
       school_sites=rep.css("h3").text.strip  
-      school=self.new(name, school_sites) 
-      @@school_list << school  
+      PianoMusicSchools::Pianists.new(name, school_sites)
+      
+       
+      @@school_list << PianoMusicSchools::Pianists.new(name, school_sites)
     end  
-    PianoMusicSchools::Pianists.new(name, school_sites)
+    
   end
   #**************************1st approach
   def self.get_piano_paragraphs(input)
@@ -129,7 +128,7 @@ class PianoMusicSchools::Scraper
 def self.data_scraper(url)
     Nokogiri::HTML(open(url))  
 end
-
+@@school_list = []
 #1 user sees the list 
 #2 selects a site from the list 
 #3 description and the link for the site
